d327355823c26458559e8bb11328cb674f1ff2f6
Philip Stathis <1742750+philipstathis@users.noreply.github.com>
2019-05-14 14:47:04 -0400

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,7 @@
-var items = ["Do it in Kickstart", "Do it in Masonry", "Write some damn tests", "Use Datadog!", "Ship it! :squirrel:", "Get Poke Bowl at Global", "Make Paaaasta", "Explain proper shoelace tying protocol", "Remove Ember Engines", "Dates are Hard", "This was already solved in Corbit", "Be ready for pain"]
+var items = ["Do it in Kickstart", "Do it in Masonry", "Write some damn tests", "Use Datadog!",
+             "Ship it! :shipit:", "Get Poke Bowl at Global", "Make Paaaasta", "Explain proper shoelace tying protocol",
+             "Remove Ember Engines", "Dates are Hard", "This was already solved in Corbit", "Be ready for pain",
+             "Think of it as a code opportunity"];
 
 var item = items[Math.floor(Math.random()*items.length)];
 
