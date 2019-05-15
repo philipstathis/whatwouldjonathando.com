815157e4b6911ef81141692731c47f0ba96ca765
RickyWan <728426+RickyWan@users.noreply.github.com>
2019-05-14 20:36:02 -0400

    Add quotes

--- quotes.js
+++ quotes.js
@@ -4,14 +4,16 @@ const quotes = [
   'Write some damn tests',
   'Use Datadog!',
   'Ship it! :shipit:',
-  'Get Poke Bowl at Global',
+  'Getting Poke at Global',
   'Make Paaaasta',
   'Explain proper shoelace tying protocol',
   'Remove Ember Engines',
   'Dates are Hard',
   'This was already solved in Corbit',
   'Be ready for pain',
-  'Think of it as a code opportunity'
+  'Think of it as a code opportunity',
+  'Hang out with his best friend Beyonce',
+  'Fridge handle'
 ];
 
 export default function getQuote() {
